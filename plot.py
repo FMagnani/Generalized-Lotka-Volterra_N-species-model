e6df4ce5e369a604ac30de4444df0e29f08375f0
Federico Magnani <federico.magnani5@studio.unibo.it>
2020-09-29 20:50:33 +0200

    Initial commit. Implemented the basic plotting feature.

---
+++ plot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 16:54:06 2020
+
+@author: fede
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+data = pd.read_csv('solutionData.csv')
+
+Species_names = list(data.columns[1:])
+N = len(Species_names)
+t = list(data['Unnamed: 0'])
+
+
+fig, ax = plt.subplots()
+for i, name in zip(range(N), Species_names):
+    nt = list(data[name])
+    ax.plot(t, nt, linewidth=4, label=name)
+
+ax.set_facecolor('white')
+ax.legend(loc='best')
